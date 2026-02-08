1badff9bfb162c87e2e0d101b3d491d5ff5745d3
Yurii Artym <140413112+yura-miyura@users.noreply.github.com>
2026-02-08 16:09:18 +0000

    started creating structure of the project

---
+++ includes/push_swap.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yartym <yartym@student.42london.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/08 15:12:46 by yartym            #+#    #+#             */
+/*   Updated: 2026/02/08 16:08:45 by yartym           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../alibft"
+
+typedef stuct s_stack
+{
+	int				number;
+	unsigned int	index;
+	struct s_stack	*previous;
+	struct s_stack	*next;
+}					t_stack
+
+
+
+
+#endif
